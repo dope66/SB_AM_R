ccdb1f4f32d9c6b813fb846e0d5fa7d50a62aefb
dope66 <prince628@naver.com>
2023-03-26 14:11:04 +0900

    header, footer, default_layout

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,9 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.repository.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
