85a3c516f161797c977a7872c662c16f21f68496
dope66 <prince628@naver.com>
2023-03-26 22:03:46 +0900

    로그인 시 에러 메세지 출력구현

--- src/main/java/com/example/demo/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/example/demo/config/CustomAuthenticationFailureHandler.java
@@ -24,11 +24,13 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException, ServletException {
+        System.out.println("자 확인 들어갑니다.");
         String errorMessage = "아이디 또는 비밀번호가 일치하지 않습니다.";
-
+        System.out.println(errorMessage);
         if (exception instanceof UsernameNotFoundException) {
             // 입력한 아이디가 존재하지 않는 경우
             errorMessage = "존재하지 않는 아이디입니다.";
+            System.out.println("아이디 점검을 하긴함?");
         } else {
             // 입력한 아이디와 데이터베이스 내의 아이디가 일치하지 않는 경우
             String username = request.getParameter("username");
@@ -38,7 +40,7 @@ public class CustomAuthenticationFailureHandler extends SimpleUrlAuthenticationF
             } catch (UsernameNotFoundException e) {
                 errorMessage = "아이디를 확인해주세요.";
                 request.getSession().setAttribute("errorMessage", errorMessage);
-                response.sendRedirect(request.getContextPath() + "/login");
+                response.sendRedirect(request.getContextPath() + "/login?error");
                 return;
             }
 
